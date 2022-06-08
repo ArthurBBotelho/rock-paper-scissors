d493c4558569696807bd5d01d68f22398e34f32c
arthur <emilioarthur19@gmail.com>
2022-06-07 21:17:44 -0300

    Success, the first challenge is completed, computerPlay generates a random value of the array 'rock paper scissors'

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
+//GET ELEMENTS
+
+
 
 //VARIABLES
 
-const move = ["Rock", "Paper", "Scissors"];
 
 function computerPlay(){
     //RANDOMLY RETURN 'ROCK PAPER SCISSORS'
+
+    const move = ["Rock", "Paper", "Scissors"];
+    let indexRandom = Math.floor (Math.random() * 3);
     
+    console.log(move[indexRandom])
+
 }
