1db5b7d9ed4f3e920b0b4f185446a7dec636ddd6
arthur <emilioarthur19@gmail.com>
2022-06-07 22:15:57 -0300

    The logic is runnig, diferenciate from user to computer, separetes which move happend and gets a winner

--- script.js
+++ script.js
@@ -3,14 +3,48 @@
 
 
 //VARIABLES
-
+const move = ["Rock", "Paper", "Scissors"];
+let userPlay = "Scissors"
+let indexRandom
+let computerMove
 
 function computerPlay(){
     //RANDOMLY RETURN 'ROCK PAPER SCISSORS'
-
-    const move = ["Rock", "Paper", "Scissors"];
-    let indexRandom = Math.floor (Math.random() * 3);
     
-    console.log(move[indexRandom])
+    indexRandom = Math.floor (Math.random() * 3);
+    computerMove = move[indexRandom]
+        
+}
 
+function whoWins(){
+    computerPlay()
+    console.log("Computer: ", computerMove)
+    console.log("User: ", userPlay)
+
+    if (computerMove === "Rock" && userPlay === "Rock") {
+    console.log("Tie")
+    } else if 
+    (computerMove === "Rock" && userPlay === "Scissors") {
+    console.log("Computer wins") 
+    } else if
+    (computerMove === "Rock" && userPlay === "Paper") {
+    console.log("User wins") 
+    } else if
+    (computerMove === "Paper" && userPlay === "Paper") {
+    console.log("Tie") 
+    } else if
+    (computerMove === "Paper" && userPlay === "Scissors") {
+    console.log("User wins") 
+    } else if
+    (computerMove === "Paper" && userPlay === "Rock") {
+    console.log("Computer wins") 
+    } else if
+    (computerMove === "Scissors" && userPlay === "Scissors") {
+    console.log("Tie") 
+    } else if
+    (computerMove === "Scissors" && userPlay === "Paper") {
+    console.log("Computer wins") 
+    } else
+    console.log("User wins") 
+    
 }
