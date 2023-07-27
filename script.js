0e9cd4984ae6294f850900fcc364353d69035ff0
Arthur <emilioarthur19@gmail.com>
2023-07-27 14:36:43 -0300

    Fazendo as funçoes

---
+++ script.js
@@ -0,0 +1,17 @@
+// Definido as variáves das jogadas
+
+const computadorMostrar = document.getElementById("computador")
+const jogadorMostrar = document.getElementById("jogador")
+const resultado = document.getElementById("resultado")
+
+const pedra = document.getElementById("pedra")
+const papel = document.getElementById("papel")
+const tesoura = document.getElementById("tesoura")
+
+let computador
+let jogador
+
+function jogadaJogador() {
+    
+}
+
