733f9d048fb540f7fdf13f38b6f870cf594582af
Arthur <emilioarthur19@gmail.com>
2023-07-28 08:42:07 -0300

    Still geting undefined result

--- script.js
+++ script.js
@@ -2,50 +2,36 @@
 
 const jogadaComputador = document.getElementById("computador")
 const jogadaJogador = document.getElementById("jogador")
-const resultadoTela = document.getElementById("resultado")
 
+let resultadoTela = document.getElementById("resultado")
 let resultado
 
 const pedra = document.getElementById("pedra")
 const papel = document.getElementById("papel")
 const tesoura = document.getElementById("tesoura")
 
-//Funções dos botões para jogador
-
-function jogadaPedra() {
-    jogadaJogador.innerHTML = "Pedra"
-}
-
-function jogadaPapel() {
-    jogadaJogador.innerHTML = "Papel"
-}
-
-function jogadaTesoura() {
-    jogadaJogador.innerHTML = "Tesoura"
-}
-
 //Função para computador jogar aleatório
 
 function computadorAleatorio() {
     const numeroAleatorio = Math.floor(Math.random() * 3) + 1
 
-    if (numeroAleatorio === 1){
+    if (numeroAleatorio == 1){
     jogadaComputador.innerHTML = "Pedra"
     }
-    if (numeroAleatorio === 2){
+    if (numeroAleatorio == 2){
     jogadaComputador.innerHTML  = "Papel"
     }
-    if (numeroAleatorio === 3){
+    if (numeroAleatorio == 3){
     jogadaComputador.innerHTML = "Tesoura"
     }
     console.log(numeroAleatorio)
     console.log("Computador: " + jogadaComputador.innerHTML)
 }
 
-//Resultado
+//Funções dos botões para jogador
 
-function jogadaResultado() {
-    
+function jogadaPedra() {
+    jogadaJogador.innerHTML = "Pedra"
     computadorAleatorio()
 
     if (jogadaJogador == "Pedra" && jogadaComputador == "Papel"){
@@ -54,13 +40,35 @@ function jogadaResultado() {
     else if (jogadaJogador == "Pedra" && jogadaComputador == "Tesoura"){
         resultado = "Voce Venceu!"
     }
-    else if (jogadaJogador == "Papel" && jogadaComputador == "Tesoura"){
+    else {
+        resultado = "Empate"
+    }
+    console.log("Resultado: " + resultado.innerHTML)
+    resultadoTela = resultado.innerHTML
+}
+
+function jogadaPapel() {
+    jogadaJogador.innerHTML = "Papel"
+    computadorAleatorio()
+
+    if (jogadaJogador == "Papel" && jogadaComputador == "Tesoura"){
         resultado = "Voce Perdeu!"
     }
     else if (jogadaJogador == "Papel" && jogadaComputador == "Pedra"){
         resultado = "Voce Venceu!"
     }
-    else if (jogadaJogador == "Tesoura" && jogadaComputador == "Pedra"){
+    else {
+        resultado = "Empate"
+    }
+    console.log("Resultado: " + resultado.innerHTML)
+    resultadoTela = resultado.innerHTML
+}
+
+function jogadaTesoura() {
+    jogadaJogador.innerHTML = "Tesoura"
+    computadorAleatorio()
+
+    if (jogadaJogador == "Tesoura" && jogadaComputador == "Pedra"){
         resultado = "Voce perdeu!"
     }
     else if (jogadaJogador == "Tesoura" && jogadaComputador == "Papel"){
@@ -69,9 +77,6 @@ function jogadaResultado() {
     else {
         resultado = "Empate"
     }
- 
-    console.log("Jogador: " + jogadaJogador.innerHTML)
     console.log("Resultado: " + resultado.innerHTML)
+    resultadoTela = resultado.innerHTML
 }
-
-
